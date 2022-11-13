f73ac0594bdfa7e2945ffc196d51f0f1c4181319
Svetly de Paula <Svetoslavadepaula@gmail.com>
2022-11-13 16:07:21 -0500

    Update stylesheet for stream-chat-react.

--- src/customStyles.js
+++ src/customStyles.js
@@ -1,11 +1,9 @@
 export const customStyles = {
-    '--border-radius-md': '0',
-    '--sm-p':'0',
-
     '--bg-gradient-end': '#101214',
     '--bg-gradient-start': '#070a0d',
     '--black': '#ffffff',
     '--blue-alice': '#59CE8F',
+    '--border-radius-md': '0',
     '--border': '#4C0033',
     '--button-background': '#ffffff',
     '--button-text': '#4C0033',
@@ -15,7 +13,9 @@ export const customStyles = {
     '--modal-shadow': '#000000',
     '--overlay': '#00000066',
     '--overlay-dark': '#ffffffcc',
+    '--primary-color':'#ffd300',
     '--shadow-icon': '#EFF5F5',
+    '--sm-p':'0',
     '--targetedMessageBackground': '#302d22',
     '--transparent': 'transparent',
     '--white': '#101418',
